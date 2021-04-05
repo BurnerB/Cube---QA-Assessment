a45de966662c91ff046426b917b135123e6802e4
Barnaby Kamau <45785786+BurnerB@users.noreply.github.com>
2021-04-05 22:44:34 +0300

    load each url in the sitemap

--- cypress/integration/sitemap_valid_status.js
+++ cypress/integration/sitemap_valid_status.js
@@ -13,8 +13,6 @@ describe('Automate Sitemap tests Ringier', () => {
                   .map(el => el.innerText);
               cy.log("all sitemaps  are " + sitemaps.length + "  " + sitemaps)
           })
-
-
   });
 
   it('sitemap index correct format', () => {
@@ -51,22 +49,18 @@ describe('Automate Sitemap tests Ringier', () => {
     });
 });
 
-  // it('should succesfully load each url in the sitemap', () => {
-  //     sitemaps.forEach(element => {
-  //         cy.log("sitemap  number " + (sitemaps.indexOf(element) + 1))
-  //         cy.request(element)
-  //             .as('sitemap')
-  //             .then((response) => {
-  //                 urls = Cypress.$(response.body)
-  //                     .find('loc')
-  //                     .toArray()
-  //                     .map(el => el.innerText);
-  //                 urls.forEach(cy.visit);
-  //             });
-
-
-  //     });
-
-  // });
-
+  it('should succesfully load each url in the sitemap', () => {
+      sitemaps.forEach(element => {
+          cy.log("sitemap  number " + (sitemaps.indexOf(element) + 1))
+          cy.request(element)
+              .as('sitemap')
+              .then((response) => {
+                  urls = Cypress.$(response.body)
+                      .find('loc')
+                      .toArray()
+                      .map(el => el.innerText);
+                  urls.forEach(cy.visit);
+              });
+      });
+  });
 });
